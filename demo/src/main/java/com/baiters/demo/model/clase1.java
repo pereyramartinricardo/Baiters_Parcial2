b5f1b48ac9586b43d7745287f8ddf3de7df3c12e
Martin <pereyramartinricardo@gmail.com>
2026-06-17 19:31:13 -0300

    Inicializar

---
+++ demo/src/main/java/com/baiters/demo/model/clase1.java
@@ -0,0 +1,5 @@
+package com.baiters.demo.model;
+
+public class clase1 {
+
+}
---
+++ demo/src/test/java/com/baiters/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiters.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
